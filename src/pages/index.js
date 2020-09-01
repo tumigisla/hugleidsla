d034dcfe830c4499f5119dbcea8ce76205e068c9
Tumi Snær Gíslason <tumi@gangverk.is>
2020-09-01 22:11:58 +0000

    Added raw skeleton for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import MeditationTimer from "../components/meditationTimer"
+import Container from "../components/container"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Container>
+      <h1>Hugleiðsla</h1>
+      <MeditationTimer
+        timerminutes="3"
+        player="Here comes a player..."
+      />
+    </Container>
+  )
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import twinPeaksTheme from "typography-theme-twin-peaks"
+
+const typography = new Typography(twinPeaksTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
