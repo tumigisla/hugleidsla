416d6a0c5552af8d343dfb1a74c4fe005bdc6a77
Tumi Snær Gíslason <tumi@gangverk.is>
2021-02-28 14:09:41 +0000

    adding react helmet dep

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Container from "../components/container"
 import Timer from "../components/timer"
+import {Helmet} from "react-helmet";
 
 export default function Home() {
   return (
     <Container>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Hugleiðsla</title>
+        <link rel="canonical" href="https://hugleidsla.tumigisla.is" />
+      </Helmet>
       <h1>Hugleiðsla</h1>
       <Timer/>
     </Container>
